2c63002c9207f9b7deb502e9303dbae6c69f3c39
Brandon Chong <phototix@hotmail.com>
2023-08-11 18:31:59 +0800

    Update doRestart.php

--- doRestart.php
+++ doRestart.php
@@ -1,6 +1,6 @@
 <?php
 // Replace '/path/to/restart.sh' with the actual path to your restart.sh script.
-$scriptPath = 'restart.sh';
+$scriptPath = './restart.sh';
 
 // Use shell_exec to run the script and capture the output (if needed).
 $output = shell_exec($scriptPath);
